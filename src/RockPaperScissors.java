9b66dfd878c33438aaa4ecce83d300f2fd7d6e3d
ritter327 <ritterma@oregonstate.edu>
2020-02-01 18:43:23 -0700

    Initial commit

---
+++ src/RockPaperScissors.java
@@ -0,0 +1,7 @@
+public class RockPaperScissors {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        //Instantiate GameManager Class member
+
+    }
+}
