d228d0739a7db1f0c8cf51eb59ef28d9c3ccc305
Cherie Young <44423254+youncher@users.noreply.github.com>
2020-02-01 19:55:20 -0800

    Added player/computer pick and game logic.

--- src/GameManager.java
+++ src/GameManager.java
@@ -6,7 +6,53 @@ public class GameManager {
         menu = new Menu();
         String playerName = menu.getName();
         System.out.println("Hello, " + playerName);
+
+        boolean gameOver = false;
+
+        while (!gameOver) {
+            menu.displayOptions();
+            int userPick = menu.getUserPick();
+            int computerPick = 2; // TODO: generate random integer between [1-3]
+            int results = findWinner(userPick, computerPick);
+
+            System.out.println("Round results: " + results);
+        }
     }
 
+    private int findWinner(int userPick, int computerPick) {
+        if (userPick == computerPick) {
+            return 3; // tie
+        } else if (userPick == 1 && computerPick == 2) {
+            return 2; // Computer wins
+        } else if (userPick == 1 && computerPick == 3) {
+            return 1; // Player wins
+        } else if (userPick == 2 && computerPick == 1) {
+            return 1; // Player wins
+        } else if (userPick == 2 && computerPick == 3) {
+            return 2; // Computer wins
+        } else if (userPick == 3 && computerPick == 1) {
+            return 2; // Computer wins
+        } else { // 3 - 2 = playerWins
+            return 1; // Player wins
+        }
+    }
 
 }
+
+// player-computer
+// 1 = rock
+// 2 = paper
+// 3 = scissors
+
+/*
+playerPick = computerPick = tie
+1 - 2 = computerWins
+1 - 3 = playerWins
+
+2 - 1 = playerWins
+2 - 3 = computerWins
+
+3 - 1 = computerWins
+3 - 2 = playerWins
+
+ */
--- src/Menu.java
+++ src/Menu.java
@@ -16,6 +16,20 @@ public class Menu {
         return playerName;
     }
 
+    public void displayOptions() {
+        System.out.println("1-Rock");
+        System.out.println("2-Paper");
+        System.out.println("3-Scissors");
+        System.out.println();
+        System.out.print("Please select an option:");
+    }
+
+    public int getUserPick() {
+        Scanner scanner = new Scanner(System.in);
+        int userPick = scanner.nextInt();
+        System.out.println("You picked: " + userPick);
+        return userPick;
+    }
 
 }
 
